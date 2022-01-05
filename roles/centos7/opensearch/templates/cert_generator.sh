46640febe1b7d8a9119e0abb926217f795a8959e
riccardomerlano <riccardo.merlano@gmail.com>
2022-01-05 15:44:29 +0100

    doc + fix

--- roles/centos7/opensearch/templates/cert_generator.sh
+++ roles/centos7/opensearch/templates/cert_generator.sh
@@ -19,9 +19,11 @@ rm {{ localhost_certs_dir }}/{{ hostname }}-key-temp.pem
 rm {{ localhost_certs_dir }}/{{ hostname }}.csr
 {% endfor %}
 # Dashboard cert
+{% if proprietary_dashboard_cert == "no_cert" %}
 openssl genrsa -out {{ localhost_certs_dir }}/dashboard-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in {{ localhost_certs_dir }}/dashboard-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out {{ localhost_certs_dir }}/dashboard-key.pem
 openssl req -new -key {{ localhost_certs_dir }}/dashboard-key.pem -subj "/C=IT/ST=LOMBARDIA/L=MILANO/O=ORG/OU=UNIT/CN=DASHBOARD" -out {{ localhost_certs_dir }}/dashboard.csr
 openssl x509 -req -in {{ localhost_certs_dir }}/dashboard.csr -CA {{ localhost_certs_dir }}/root-ca.pem -CAkey {{ localhost_certs_dir }}/root-ca-key.pem -CAcreateserial -sha256 -out {{ localhost_certs_dir }}/dashboard.pem -days 730
 rm {{ localhost_certs_dir }}/dashboard-key-temp.pem
 rm {{ localhost_certs_dir }}/dashboard.csr
+{% endif %}
